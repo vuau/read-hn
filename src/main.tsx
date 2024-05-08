eb0126a98fcf2710a02998f39e3d48ec38d3a3f6
Au <phamvuau@gmail.com>
2024-05-08 22:28:48 +0700

    improve css

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,8 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
 import "@reach/dialog/styles.css";
+import './water.css';
 import { inject } from '@vercel/analytics';
-import "./App.css";
 
 inject();
 const queryClient = new QueryClient()
